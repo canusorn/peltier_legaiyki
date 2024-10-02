3aa5160e19ef9f121b3169daff36278d2d656bb0
canusorn <39732595+canusorn@users.noreply.github.com>
2024-10-02 19:35:50 +0700

    Update peltier.ino

--- peltier/peltier.ino
+++ peltier/peltier.ino
@@ -1,4 +1,16 @@
-#define GSHEETUPDATE 60   // update in second
+/*
+
+    SPI   MOSI    MISO    CLK     CS
+   VSPI  GPIO23  GPIO19  GPIO18  GPIO5
+*/
+
+
+
+#define VOLT_PIN 34   /// 0-4095
+#define HEATTER_PIN 13
+#define HEATTER_CONTROL 4
+#define HEATTER_TEMP 39  
+#define GSHEETUPDATE 30   // update in second
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
@@ -20,27 +32,32 @@
 char ssid[] = "G6PD";
 char pass[] = "570610193";
 
-const int ThermoCouplesNum = 1;
+const int ThermoCouplesNum = 4;
 MAX6675 ThermoCouples[ThermoCouplesNum] =
 {
-  MAX6675(5, &SPI),   //  HW SPI
+  MAX6675(33, &SPI),   //  HW SPI
+  MAX6675(25, &SPI),   //  HW SPI
+  MAX6675(26, &SPI),   //  HW SPI
+  MAX6675(27, &SPI),   //  HW SPI
 };
 
 unsigned long previousMillis = 0;        // will store last time LED was updated
 uint16_t gsheetInterval;
-float temp[4];
-float volt = 2.1;
+float temp[4], volt;
 
 void setup()
 {
   Serial.begin(115200);
+
+  digitalWrite(HEATTER_PIN, HIGH);
+  pinMode(HEATTER_PIN, OUTPUT);
 
   SPI.begin();
 
   for (int i = 0; i < ThermoCouplesNum; i++)
   {
     ThermoCouples[i].begin();
-    ThermoCouples[i].setSPIspeed(4000000);
+    ThermoCouples[i].setSPIspeed(1000000);
   }
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
@@ -56,27 +73,43 @@ void loop()
     previousMillis = currentMillis;
 
     gsheetInterval++;
-
+    volt = 0;
     for (int THCnumber = 0; THCnumber < ThermoCouplesNum; THCnumber++)
     {
       int status = ThermoCouples[THCnumber].read();
+      Serial.print("Status" + String(status));
       float t = ThermoCouples[THCnumber].getTemperature();
       temp[THCnumber] = t;
-      Serial.print("temp" + String(THCnumber) + ": ");
+      Serial.print("\ttemp" + String(THCnumber + 1) + ": ");
       Serial.println(t);
+      volt += analogRead(VOLT_PIN);
 
-      //      delay(100);  //  time to flush all Serial stuff
+      delay(100);  //  time to flush all Serial stuff
     }
+
+    volt = volt / ThermoCouplesNum;
+    Serial.println("Volt:" + String(volt));
+
+    if (temp[HEATTER_CONTROL - 1] > HEATTER_TEMP) {
+      if(digitalRead(HEATTER_PIN) == LOW) Serial.println("heater on");
+      digitalWrite(HEATTER_PIN, HIGH );
+    } else if (temp[HEATTER_CONTROL - 1] < HEATTER_TEMP - 1) {
+      if(digitalRead(HEATTER_PIN) == HIGH) Serial.println("heater off");
+      digitalWrite(HEATTER_PIN, LOW);
+    }
+
+    Serial.println("------------------");
 
     Blynk.virtualWrite(V0, temp[0]);
     Blynk.virtualWrite(V1, temp[1]);
     Blynk.virtualWrite(V2, temp[2]);
     Blynk.virtualWrite(V3, temp[3]);
+    Blynk.virtualWrite(V4, volt);
 
     if (gsheetInterval >= GSHEETUPDATE) {
       gsheetInterval = 0;
-gsheet();
       Serial.println("gsheet update");
+      gsheet();
     }
   }
 }
